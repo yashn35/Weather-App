e93f6cf8bff590337dd359fd1c42a8078e2844aa
yash-narayan <yasnara04@gmail.com>
2017-12-25 22:10:27 -0800

    commiting entire project

--- rainyshinyapp/WeatherVC.swift
+++ rainyshinyapp/WeatherVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherVC.swift
 //  rainyshinyapp
 //
 //  Created by AMIT NARAYAN on 3/2/17.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
